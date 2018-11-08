844fe935bdfd5a2619517678435839509f2864a7
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-08 16:47:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
 class App < Sinatra::Base
+  get "/" do
+    erb :index
+  end
+
+  post "/checkout" do
+    @session = session
+    @session[:item] = params[:item]
+    erb :checkout
+  end
 end
